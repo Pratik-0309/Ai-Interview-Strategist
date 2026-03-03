18b674147ad2d750e6196e129415d59bc1d22187
PratikParate <paratepratik10@gmail.com>
2026-03-03 16:32:28 +0530

    setup Express server with MongoDB connection

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+
+app.listen(8080, ()=>{
+    console.log(`Server is running on port 8080`);
+})
+

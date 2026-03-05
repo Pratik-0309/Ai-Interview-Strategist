49fb9453dc6c33998eee95de6faf1b9fa47adb13
PratikParate <paratepratik10@gmail.com>
2026-03-05 17:45:41 +0530

    add method to generate access ,refresh token and hash, compare password

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,22 +1,61 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        unique: [true, "Username Already taken"],
-        required: true
-    },
-    email: {
-        type: String,
-        unique: [true, "Account Already exist with this email"],
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})  
+  userName: {
+    type: String,
+    unique: [true, "Username Already taken"],
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: [true, "Account Already exist with this email"],
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const User = mongoose.model("User",userSchema);
+// method to hash password
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// method to match password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// method to generate access token
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+};
+
+// method to generate refresh token
+userSchema.method.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+};
+
+const User = mongoose.model("User", userSchema);
 
 export default User;

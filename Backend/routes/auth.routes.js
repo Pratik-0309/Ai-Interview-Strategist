a263a2e38dc3112de8160d6da1aa38b3aa78fbfe
PratikParate <paratepratik10@gmail.com>
2026-03-07 10:27:51 +0530

    add authenticated /profile & /logout routes

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { registerUser, loginUser, logoutUser, refreshAccessToken} from '../controllers/auth.controller.js';
+import { registerUser, loginUser, logoutUser,userProfile, refreshAccessToken} from '../controllers/auth.controller.js';
+import verifyJWT from "../middleware/auth.middleware.js"
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerUser);
-authRouter.post("/login", loginUser);
+authRouter.post("/login", loginUser);   
 authRouter.post("/refresh-token", refreshAccessToken);
-authRouter.post("/logout", logoutUser);
+authRouter.get("/profile",verifyJWT, userProfile);
+authRouter.post("/logout",verifyJWT ,logoutUser);
 
 export default authRouter;

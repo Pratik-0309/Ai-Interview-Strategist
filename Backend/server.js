8cb57b7ffc8fb91ce458329712ee7c1c3064a596
PratikParate <paratepratik10@gmail.com>
2026-03-07 10:25:47 +0530

    add cookie-parser middelware & authRouter endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -9,7 +11,9 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth",authRouter);
 
 app.listen(8080, ()=>{
     console.log(`Server is running on port 8080`);

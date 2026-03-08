bcb2d7291a625bdfd988c3978e398b257a33e860
PratikParate <paratepratik10@gmail.com>
2026-03-08 18:13:47 +0530

    remove StrictMode and wrap app component inside AuthProvider for Props drilling

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Features/auth/auth.context.jsx";
 import App from "./App.jsx";
 import "./style.scss";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </AuthProvider>
+  </BrowserRouter>,
 );

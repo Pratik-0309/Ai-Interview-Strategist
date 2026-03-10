cb4e929cc0d855dda7ea473c1fb4ed0b0e6e1054
PratikParate <paratepratik10@gmail.com>
2026-03-10 12:59:35 +0530

    add protected / (home) route & Toaster

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Register from "./Features/auth/pages/Register.jsx"
 import Login from "./Features/auth/pages/Login.jsx"
+import Protected from "./Features/auth/components/Protected.jsx";
+import {Toaster} from "react-hot-toast";
+
 
 function App() {
 
   return (
     <>
+    <Toaster />
     <Routes>
+      <Route path="/" element={<Protected><h1>Home Page</h1></Protected>} />
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
     </Routes>

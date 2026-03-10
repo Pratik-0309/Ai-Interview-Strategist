a3b2f17d3733ea11299ae2536182454241967c69
PratikParate <paratepratik10@gmail.com>
2026-03-10 12:58:27 +0530

    add cors configuration to connect server with client

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from 'cookie-parser';
+import cors from "cors"
 
 dotenv.config();
 
@@ -12,6 +13,10 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth",authRouter);
 

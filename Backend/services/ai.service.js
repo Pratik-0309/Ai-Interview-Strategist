cf632e6176712d90ba9a42a5ddcc17725c40f6ce
PratikParate <paratepratik10@gmail.com>
2026-03-11 18:56:09 +0530

    add api env variable instead of actual key api

--- Backend/services/ai.service.js
+++ Backend/services/ai.service.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyAkv15qhQZ4RqwvEiFu2lMw9hpsPMtYYDU");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENAI_API_KEY);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-3.1-flash-lite-preview", 

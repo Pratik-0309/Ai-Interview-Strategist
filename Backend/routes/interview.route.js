967c391065d8797664cb6847be95370bd2eeda3b
PratikParate <paratepratik10@gmail.com>
2026-03-14 23:28:20 +0530

    add routes to get interview report by id and user reports

--- Backend/routes/interview.route.js
+++ Backend/routes/interview.route.js
@@ -1,11 +1,13 @@
 import express from "express"
 import verifyJWT from "../middleware/auth.middleware.js";
-import {generateInterviewReportController} from "../controllers/interview.controller.js"
+import {generateInterviewReportController, getInterviewReportById, getUserInterviewReport} from "../controllers/interview.controller.js"
 import upload from "../middleware/multer.middleware.js"
 
 const interviewRouter = express.Router();
 
 interviewRouter.post("/", verifyJWT,upload.single("resume"), generateInterviewReportController);
+interviewRouter.get("/report/:interviewId", verifyJWT, getInterviewReportById);
+interviewRouter.get("/", verifyJWT, getUserInterviewReport);
 
 
 export default interviewRouter;

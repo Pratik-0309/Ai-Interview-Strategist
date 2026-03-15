e993be81e8638d1f96ce6af74dc9e1e98ccbf178
PratikParate <paratepratik10@gmail.com>
2026-03-15 12:25:15 +0530

    wrap App in InterviewProvider for global interview state

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Features/auth/auth.context.jsx";
+import { InterviewProvider } from "./Features/interview/interview.context.jsx";
 import App from "./App.jsx";
 import "./style.scss";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <InterviewProvider>
+        <App />
+      </InterviewProvider>
     </AuthProvider>
   </BrowserRouter>,
 );
